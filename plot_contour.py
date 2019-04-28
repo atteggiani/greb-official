ddff05f1607f88069cca790b218562961db2c88d
atteggiani <davide.marchegiani@gmail.com>
2019-04-29 02:02:55 +1000

    plot_contour completed

--- plot_contour.py
+++ plot_contour.py
@@ -12,9 +12,188 @@ import iris.plot as iplt
 import cartopy.crs as ccrs
 cdo=Cdo()
 
-# FUNCTIONS //==============================================//
+# CLASSES and FUNCTIONS //==============================================//
+def annual_mean(cubes):
+    # Create annual mean
+    fl=False
+    if not isinstance(cubes,list):
+        cubes = [cubes]
+        fl=True
+    # get var names
+    varnames=[var.var_name for var in cubes]
+    # compute annual mean
+    amean=[iris.util.squeeze(var.collapsed('time',iris.analysis.MEAN)) \
+                                                            for var in cubes]
+    # set var names
+    for var,name in zip(amean,varnames):
+        var.var_name = name+'.amean'
+    return amean[0] if fl else amean
+
+def seasonal_cycle(cubes):
+    # Create seasonal cycle (DJF-JJA)
+    fl=False
+    if not isinstance(cubes,list):
+        cubes = [cubes]
+        fl=True
+    # get var names
+    varnames=[var.var_name for var in cubes]
+    # add seasons
+    [iris.coord_categorisation.add_season(var, 'time', name='seasons',
+     seasons=('djf', 'mam', 'jja', 'son')) for var in cubes if \
+     'seasons' not in [coord.name() for coord in var.coords()]]
+    # compute seasonal cycle
+    seasonmean = [iris.util.squeeze(var.aggregated_by('seasons',
+                                        iris.analysis.MEAN)) for var in cubes]
+    djf = [var.extract(iris.Constraint(seasons='djf')) for var in seasonmean]
+    jja = [var.extract(iris.Constraint(seasons='jja')) for var in seasonmean]
+    cycle = [var1-var2 for var1,var2 in zip(djf,jja)]
+    # set var name
+    for var,name in zip(cycle,varnames):
+        var.var_name = name+'.seascyc'
+    return cycle[0] if fl else cycle
+
+class plot_param:
+    ext = 'png'
+    def __init__(self, cube = None, units = None, cmap = None, cmaplev = None,
+                 cbticks = None, defname = None, varname = None, tit = None):
+        self.cube = cube
+        self.cmap = cmap
+        self.cmaplev = cmaplev
+        self.cbticks = cbticks
+        self.units = units
+        self.defname = defname
+        self.varname = varname
+        self.tit = tit
+
+    def __str__(self):
+        return str(print(self.cube))
+
+    def set_cube(self,cube):
+        self.cube = cube
+
+    def set_units(self,units):
+        self.units = units
+
+    def set_cmap(self,cmap):
+        self.cmap = cmap
+
+    def set_cmaplev(self,cmaplev):
+        self.cmaplev = cmaplev
+
+    def set_cbticks(self,cbticks):
+        self.cbticks = cbticks
+
+    def set_varname(self,varname):
+        self.varname = varname
+
+    def set_tit(self,tit):
+        self.tit = tit
+
+    def get_cube(self):
+        return self.cube
+
+    def get_units(self):
+        return self.units
+
+    def get_cmap(self):
+        return self.cmap
+
+    def get_cmaplev(self):
+        return self.cmaplev
+
+    def get_cbticks(self):
+        return self.cbticks
+
+    def get_defname(self):
+        return self.defname
+
+    def get_varname(self):
+        return self.varname
+
+    def get_tit(self):
+        return self.tit
+
+    def plot(self, outpath = None, dpi = 300):
+        plt.figure(figsize=(12, 8))
+        plt.subplot(111,projection=ccrs.Robinson())
+        iplt.contourf(self.cube, levels = self.cmaplev, cmap = self.cmap,
+                      extend='both')
+        plt.gca().coastlines()
+        plt.colorbar(orientation='horizontal',extend='both',label = self.units,
+                    ticks = self.cbticks)
+        plt.title(self.tit)
+        # iplt.citation(name)
+        if outpath is not None:
+            plt.savefig(os.path.join(outpath,'.'.join([self.varname,self.ext])),
+                        format = self.ext, dpi = dpi)
+
+    @classmethod
+    def set_ext(cls,ext):
+        cls.ext = ext
+
+    @classmethod
+    def from_cube(cls, cube, units = None, cmap = cm.RdBu, cmaplev = None,
+                  cbticks = None, varname = None, tit = None):
+        units = cube.units if units is None else units
+        cmaplev = cmap.N if cmaplev is None else cmaplev
+        varname = cube.var_name if varname is None else varname
+        tit = cube.var_name if tit is None else tit
+
+        return cls(cube = cube, units = units, cmap = cmap, cmaplev = cmaplev,
+                   cbticks = cbticks, defname = cube.var_name,
+                   varname = varname, tit = tit)
+    @classmethod
+    def annual_mean(cls, cube, units = None, cmap = cm.RdBu, cmaplev = None,
+                  cbticks = None, varname = None, tit = None):
+        defname = cube.var_name
+        cube = annual_mean(cube)
+        units = cube.units if units is None else units
+        cmaplev = cmap.N if cmaplev is None else cmaplev
+        varname = cube.var_name if varname is None else varname
+        tit = cube.var_name if tit is None else tit
+
+        return cls(cube = cube, units = units, cmap = cmap, cmaplev = cmaplev,
+                   cbticks = cbticks, defname = defname, varname = varname,
+                   tit = tit)
+
+    @classmethod
+    def seasonal_cycle(cls, cube, units = None, cmap = cm.RdBu, cmaplev = None,
+                       cbticks = None, varname = None, tit = None):
+        defname = cube.var_name
+        cube = seasonal_cycle(cube)
+        units = cube.units if units is None else units
+        cmaplev = cmap.N if cmaplev is None else cmaplev
+        varname = cube.var_name if varname is None else varname
+        tit = cube.var_name if tit is None else tit
+
+        return cls(cube = cube, units = units, cmap = cmap, cmaplev = cmaplev,
+                   cbticks = cbticks, defname = defname, varname = varname,
+                   tit = tit)
+
+    def to_annual_mean(self):
+        tit = self.get_tit()
+        cube = self.get_cube()
+        newcube = annual_mean(cube)
+        self.set_cube(newcube)
+        self.set_varname(newcube.var_name)
+        if newcube.var_name == tit + '.amean':
+            self.set_tit(newcube.var_name)
+        else: self.set_tit(tit)
+        return self
+
+    def to_seasonal_cycle(self):
+        tit = self.get_tit()
+        cube = self.get_cube()
+        newcube = seasonal_cycle(cube)
+        self.set_cube(newcube)
+        self.set_varname(newcube.var_name)
+        if newcube.var_name == tit + '.seascyc':
+            self.set_tit(newcube.var_name)
+        else: self.set_tit(tit)
+        return self
+
 def parsevar(cubes):
-    # Correct values of output variables
+    # Initialize output variables
     fl=False
     if not isinstance(cubes,list):
         cubes = [cubes]
@@ -23,50 +202,154 @@ def parsevar(cubes):
     for var in cubes:
         var.long_name = None
     # PRECIP
-    if 'precip' in varnames:
-        ind = varnames.index('precip')
-        cubes[ind]*=-1
-        cubes[ind].var_name='precip'
+    id = 'precip'
+    if id in varnames:
+        ind = varnames.index(id)
+        cubes[ind]*=-1*86400
+        cubes[ind].var_name=id
+        cubes[ind].units='mm/day'
+    # EVA
+    id = 'eva'
+    if id in varnames:
+        ind = varnames.index(id)
+        cubes[ind]*=86400
+        cubes[ind].var_name=id
+        cubes[ind].units='mm/day'
+    # QCRCL
+    id = 'qcrcl'
+    if id in varnames:
+        ind = varnames.index(id)
+        cubes[ind]*=86400
+        cubes[ind].var_name=id
+        cubes[ind].units='mm/day'
     return cubes[0] if fl else cubes
 
-def annual_mean(cubes):
-    # Create annual mean
-
+def plot_annual_mean(cubes,outpath=None):
+    # Initialize output variables
     fl=False
     if not isinstance(cubes,list):
         cubes = [cubes]
         fl=True
-    # get var names
     varnames=[var.var_name for var in cubes]
-    # compute annual mean
-    amean=[iris.util.squeeze(var.collapsed('time',iris.analysis.MEAN)) for var in cubes]
-    # set var names
-    for var,name in zip(amean,varnames):
-        var.var_name = name+'.amean'
-    return amean[0] if fl else amean
+    txt = ' Annual Mean'
+    # TSURF
+    id = 'tsurf'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind], cmap = cm.RdBu_r,
+                             cmaplev = np.arange(213,333+5,5),
+                             cbticks = np.arange(213,333+10,10),
+                             tit = 'Surface Temperature'+txt). \
+                             to_annual_mean().plot(outpath)
+    # TATMOS
+    id = 'tatmos'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind], cmap = cm.RdBu_r,
+                             cmaplev = np.arange(213,333+5,5),
+                             cbticks = np.arange(213,333+10,10),
+                             tit = 'Atmospheric Temperature'+txt). \
+                             to_annual_mean().plot(outpath)
+    # TOCEAN
+    id = 'tocean'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind], cmap = cm.Reds,
+                             cmaplev = np.arange(273,323+5,5),
+                             cbticks = np.arange(273,333+10,10),
+                             tit = 'Ocean Temperature'+txt). \
+                             to_annual_mean().plot(outpath)
+    # PRECIP
+    id = 'precip'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind], cmap = cm.GnBu,
+                             cmaplev = np.arange(0,15),
+                             cbticks = np.arange(0,17,2),
+                             tit = 'Precipitation'+txt). \
+                             to_annual_mean().plot(outpath)
+    # EVA
+    id = 'eva'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind],
+                             cmaplev = np.arange(-8,9),
+                             cbticks = np.arange(-8,10,2),
+                             tit = 'Evaporation'+txt). \
+                             to_annual_mean().plot(outpath)
+    # QCRCL
+    id = 'qcrcl'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind],
+                             cmaplev = np.arange(-8,9),
+                             cbticks = np.arange(-8,10,2),
+                             tit = 'Circulation'+txt). \
+                             to_annual_mean().plot(outpath)
 
-def seasonal_cycle(cubes):
-    # Create seasonal cycle (DJF-JJA)
-
+def plot_seasonal_cycle(cubes,outpath=None):
+    # Initialize output variables
     fl=False
     if not isinstance(cubes,list):
         cubes = [cubes]
         fl=True
-    # get var names
     varnames=[var.var_name for var in cubes]
-    # add seasons
-    [iris.coord_categorisation.add_season(var, 'time', name='seasons', seasons=('djf', 'mam', 'jja', 'son')) for var in cubes]
-    # compute seasonal cycle
-    seasonmean = [iris.util.squeeze(var.aggregated_by('seasons',iris.analysis.MEAN)) for var in cubes]
-    djf = [var.extract(iris.Constraint(seasons='djf')) for var in seasonmean]
-    jja = [var.extract(iris.Constraint(seasons='jja')) for var in seasonmean]
-    cycle = [var1-var2 for var1,var2 in zip(djf,jja)]
-    # set var names
-    for var,name in zip(cycle,varnames):
-        var.var_name = name+'.seascyc'
-    return cycle[0] if fl else cycle
+    txt = ' Seasonal Cycle'
+    # TSURF
+    id = 'tsurf'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind], cmap = cm.RdBu_r, units = '°C',
+                             cmaplev = np.arange(-50,50+5,5),
+                             cbticks = np.arange(-50,50+10,10),
+                             tit = 'Surface Temperature'+txt). \
+                             to_seasonal_cycle().plot(outpath)
+    # TATMOS
+    id = 'tatmos'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind], cmap = cm.RdBu_r, units = '°C',
+                             cmaplev = np.arange(-50,50+5,5),
+                             cbticks = np.arange(-50,50+10,10),
+                             tit = 'Atmospheric Temperature'+txt). \
+                             to_seasonal_cycle().plot(outpath)
+    # TOCEAN
+    id = 'tocean'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind], cmap = cm.RdBu_r, units = '°C',
+                             cmaplev = np.arange(-0.35,0.35+0.05,0.05),
+                             cbticks = np.arange(-0.35,0.35+0.05,0.1),
+                             tit = 'Ocean Temperature'+txt). \
+                             to_seasonal_cycle().plot(outpath)
+    # PRECIP
+    id = 'precip'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind],
+                             cmaplev = np.arange(-10,11),
+                             cbticks = np.arange(-10,10+2,2),
+                             tit = 'Precipitation'+txt). \
+                             to_seasonal_cycle().plot(outpath)
+    # EVA
+    id = 'eva'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind],
+                             cmaplev = np.arange(-10,11),
+                             cbticks = np.arange(-10,10+2,2),
+                             tit = 'Evaporation'+txt). \
+                             to_seasonal_cycle().plot(outpath)
+    # QCRCL
+    id = 'qcrcl'
+    if id in varnames:
+        ind = varnames.index(id)
+        plot_param.from_cube(cube = cubes[ind],
+                             cmaplev = np.arange(-10,11),
+                             cbticks = np.arange(-10,10+2,2),
+                             tit = 'Circulation'+txt). \
+                             to_seasonal_cycle().plot(outpath)
 
-# pltvar  = ('tsurf', 'tatmos', 'tocean', 'precip', 'eva', 'qcrcl')
 # units = ('K','K','K','kg m^-2 s^-1','kg m^-2 s^-1','kg m^-2 s^-1')
 # Reading file name
 # filename = sys.argv[1]
@@ -79,180 +362,12 @@ outdir=os.path.join(os.getcwd(),'figures',name)
 os.makedirs(outdir,exist_ok=True)
 
 # Converting bin file to netCDF
-cdo.import_binary(input = filename+'.ctl',    output = outfile,  options = '-f nc')
+cdo.import_binary(input = filename+'.ctl', output = outfile, options = '-f nc')
 
 # Importing the data cube
 data = iris.load(outfile)
 data = parsevar(data)
-# varnames=[var.var_name for var in data]
 
-# Create annual mean
-amean=annual_mean(data)
-
-# Create seasonal cycle (DJF-JJA)
-cycle=seasonal_cycle(data)
-
-# # plot annual mean
-# cmap=cm.seismic
-# ext = 'png'
-# for var in amean:
-#     plt.figure(figsize=(12, 8))
-#     plt.subplot(111,projection=ccrs.Robinson())
-#     iplt.contourf(var, 20,cmap=cmap)
-#     # Add coastlines to the map
-#     plt.gca().coastlines()
-#     plt.colorbar(orientation='horizontal',extend='both')
-#     plt.title(var.var_name)
-#     outname = '.'.join([var.var_name,'amean',ext])
-#     iplt.citation(name)
-#     plt.savefig(os.path.join(outdir,outname), format=ext, dpi=300)
-
-# Plot annual mean for 'PRECIP'
-ext = 'png'
-ind=[var.var_name for var in data].index('precip')
-var=amean[ind]*86400
-units = 'mm/day'
-cmaplev = np.arange(0,12)
-cbticks = np.arange(0,12,2)
-cmap = cm.GnBu
-varname = 'precip'
-tit = 'Precipitation'
-
-plt.figure(figsize=(12, 8))
-plt.subplot(111,projection=ccrs.Robinson())
-iplt.contourf(var, levels = cmaplev, cmap=cmap, extend='both')
-plt.gca().coastlines()
-plt.colorbar(orientation='horizontal',extend='both',label = units,
-            ticks=cbticks)
-plt.title(tit)
-iplt.citation(name)
-outname = '.'.join([varname,'amean',ext])
-plt.savefig(os.path.join(outdir,outname), format=ext, dpi=300)
-
-# Plot annual mean for 'EVA'
-ext = 'png'
-ind=[var.var_name for var in data].index('eva')
-var=amean[ind]*86400
-units = 'mm/day'
-cmaplev = np.arange(-8,9)
-cbticks = np.arange(-8,10,2)
-cmap = cm.bwr_r
-varname = 'eva'
-tit = 'Evaporation'
-
-plt.figure(figsize=(12, 8))
-plt.subplot(111,projection=ccrs.Robinson())
-iplt.contourf(var, levels = cmaplev, cmap=cmap, extend='both')
-plt.gca().coastlines()
-plt.colorbar(orientation='horizontal',extend='both',label = units,
-            ticks=cbticks)
-plt.title(tit)
-iplt.citation(name)
-outname = '.'.join([varname,'amean',ext])
-plt.savefig(os.path.join(outdir,outname), format=ext, dpi=300)
-
-# Plot annual mean for 'CRCL'
-ext = 'png'
-ind=[var.var_name for var in data].index('qcrcl')
-var=amean[ind]*86400
-units = 'mm/day'
-cmaplev = np.arange(-8,9)
-cbticks = np.arange(-8,10,2)
-cmap = cm.bwr_r
-varname = 'qcrcl'
-tit = 'Circulation'
-
-plt.figure(figsize=(12, 8))
-plt.subplot(111,projection=ccrs.Robinson())
-iplt.contourf(var, levels = cmaplev, cmap=cmap, extend='both')
-plt.gca().coastlines()
-plt.colorbar(orientation='horizontal',extend='both',label = units,
-            ticks=cbticks)
-plt.title(tit)
-iplt.citation(name)
-outname = '.'.join([varname,'amean',ext])
-plt.savefig(os.path.join(outdir,outname), format=ext, dpi=300)
-
-# ===============================================================
-# ===============================================================
-
-# Plot seasonal cycle for 'PRECIP'
-ext = 'png'
-ind=[var.var_name for var in cycle].index('precip_seascyc')
-var=cycle[ind]*86400
-units = 'mm/day'
-cmaplev = np.arange(-5,6)
-cbticks = np.arange(-4,6,2)
-cmap = cm.bwr_r
-varname = 'precip'
-tit = 'Precipitation Seasonal Cycle'
-plt.figure(figsize=(12, 8))
-plt.subplot(111,projection=ccrs.Robinson())
-iplt.contourf(var, levels = cmaplev, cmap=cmap, extend='both')
-plt.gca().coastlines()
-plt.colorbar(orientation='horizontal',extend='both',label = units,
-            ticks=cbticks)
-plt.title(tit)
-iplt.citation(name)
-outname = '.'.join([varname,'seascyc',ext])
-plt.savefig(os.path.join(outdir,outname), format=ext, dpi=300)
-
-# Plot seasonal cycle for 'EVA'
-ext = 'png'
-ind=[var.var_name for var in cycle].index('eva_seascyc')
-var=cycle[ind]*86400
-units = 'mm/day'
-cmaplev = np.arange(-8,9)
-cbticks = np.arange(-8,10,2)
-cmap = cm.bwr_r
-varname = 'eva'
-tit = 'Evaporation seasonal cycle'
-
-plt.figure(figsize=(12, 8))
-plt.subplot(111,projection=ccrs.Robinson())
-iplt.contourf(var, levels = cmaplev, cmap=cmap, extend='both')
-plt.gca().coastlines()
-plt.colorbar(orientation='horizontal',extend='both',label = units,
-            ticks=cbticks)
-plt.title(tit)
-iplt.citation(name)
-outname = '.'.join([varname,'seascyc',ext])
-plt.savefig(os.path.join(outdir,outname), format=ext, dpi=300)
-
-# Plot annual mean for 'QCRCL'
-ext = 'png'
-ind=[var.var_name for var in cycle].index('qcrcl_seascyc')
-var=cycle[ind]*86400
-units = 'mm/day'
-cmaplev = np.arange(-8,9)
-cbticks = np.arange(-8,10,2)
-cmap = cm.bwr_r
-varname = 'qcrcl'
-tit = 'Circulation seasonal cycle'
-
-plt.figure(figsize=(12, 8))
-plt.subplot(111,projection=ccrs.Robinson())
-iplt.contourf(var, levels = cmaplev, cmap=cmap, extend='both')
-plt.gca().coastlines()
-plt.colorbar(orientation='horizontal',extend='both',label = units,
-            ticks=cbticks)
-plt.title(tit)
-iplt.citation(name)
-outname = '.'.join([varname,'seascyc',ext])
-plt.savefig(os.path.join(outdir,outname), format=ext, dpi=300)
-
-# Deleting netCDF file after analysis
-# os.remove(outfile)
-
-# def parsecvar(varlist):
-#     pltvar  = ('tsurf', 'tatmos', 'tocean', 'precip', 'eva', 'qcrcl')
-#     ind = [pltvar.index(var) if var in pltvar else np.nan for var in varlist]
-#     # if var not in pltvar:
-#     #     Warning("'"+var+"' is not a recognized variable")
-#     return ind
-#
-# # Settings for 'precip'
-#     units = 'mm/day'
-#     name = 'Precipitation'
-#     coeff = 86400
-#     bounds = [0,2,4,6,8,10]
+#  Plotting data countour
+plot_annual_mean(data,outpath=outdir)
+plot_seasonal_cycle(data,outpath=outdir)
